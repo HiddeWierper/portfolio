e4dca08edc7b83258c3efde870774de74cee0e48
HiddeWierper <hidde@wierper.net>
2023-12-10 22:00:38 +0100

    Refactor CSS styles and update social media links and greetings section

--- index.php
+++ index.php
@@ -11,15 +11,49 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Rethink+Sans&family=Roboto:wght@300&display=swap" rel="stylesheet">
+  <link rel="icon" href="img/favicon.png" type="image/x-icon">
 </head>
 <body>
 <?php include 'php/header.php'; ?>
 
 <div class="socials">
-  <i class="fa-brands fa-github"></i>
-  <i class="fa-brands fa-twitter"></i>
-  <i class="fa-brands fa-instagram"></i>
-  <i class="fa-brands fa-spotify"></i>
+  <a href="https://github.com/HiddeWierper" class="fa-brands fa-github"></a>
+  <a href="https://twitter.com/Hidde_Wierper" class="fa-brands fa-twitter"></a>
+  <a href="https://instagram.com/hiddewierper" class="fa-brands fa-instagram"></a>
+  <a href="https://open.spotify.com/user/213ang5atptpcmibq5zug3w4q?si=c9ce2137287e4ebb" class="fa-brands fa-spotify"></a>
+</div>
+
+<div class="greetings">
+  <h1>𝗛𝗜, 𝗜'𝗠 <strong>𝗛𝗜𝗗𝗗𝗘 𝗪𝗜𝗘𝗥𝗣𝗘𝗥<strong></h1>
+  <p>I'm a 16 year old student from The Netherlands. 
+    I am currently studying in Almere to become a software developer</p>
+</div>
+
+<div class="skills">
+  <h1>𝗠𝗬 𝗦𝗞𝗜𝗟𝗟𝗦</h1>
+  <div class="skill">
+    <img src="img/html.png" alt="HTML">
+    <h2>HTML</h2>
+  </div>
+  <div class="skill">
+    <img src="img/css.png" alt="CSS">
+    <h2>CSS</h2>
+  </div>
+  <div class="skill">
+    <img src="img/js.png" alt="JavaScript">
+    <h2>JavaScript</h2>
+  </div>
+  <div class="skill">
+    <img src="img/php.png" alt="PHP">
+    <h2>PHP</h2>
+  </div>
+  <div class="skill">
+    <img src="img/sql.png" alt="SQL">
+    <h2>SQL</h2>
+  </div>
+  <div class="skill">
+    <img src="img/java.png" alt="Java">
+    <h2>Java</h2>
   
 </div>
 
