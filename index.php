96dd0f15a493e2947233a0a34c81deb7c90d4034
HiddeWierper <hidde@wierper.net>
2023-12-10 18:04:47 +0100

    Update code formatting and fix minor bugs

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Rethink+Sans&family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+<?php include 'php/header.php'; ?>
+
+<div class="socials">
+  <i class="fa-brands fa-github"></i>
+  <i class="fa-brands fa-twitter"></i>
+  <i class="fa-brands fa-instagram"></i>
+  <i class="fa-brands fa-spotify"></i>
+  
+</div>
+
+
+<script src="https://kit.fontawesome.com/c6d023de9c.js" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,11 @@
+<header>
+  <span class="me"><img src="img/me.jpeg" alt=""><h2>Hidde Wierper</h2></span>
+  <span class="nav">
+  <ul>
+    <a href=""><li>HOME</li></a>
+    <a href=""><li>ABOUT</li></a>
+    <a href=""><li>PROJECTS</li></a>
+    <a href=""><li>CONTACT</li></a>
+  </ul>
+  </span>
+</header>
