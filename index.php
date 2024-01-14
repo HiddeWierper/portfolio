ea8e8e8d076d1a86d20fbdf2470ea48fa9df2cd9
HiddeWierper <144018500+HiddeWierper@users.noreply.github.com>
2024-01-14 21:19:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if($_SERVER['SERVER_NAME'] == 'localhost') {
   $password = 'root';
   $username = 'root';
 }else if($_SERVER['SERVER_NAME'] == 'thuis.wierper.net') {
-  $servername = 'thuis.wierper.net';
+  $hostname = 'thuis.wierper.net';
   $password = 'Wierper1411';
   $username = 'root';
   
